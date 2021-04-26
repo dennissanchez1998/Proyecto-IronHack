fcc3d6deb5ef5b31131ee4b90ae97d97d9b174b1
dennis <dsanchez@unpluggedcorp.com>
2021-04-25 21:20:34 -0400

    nave disparando

--- js/game.js
+++ js/game.js
@@ -1,13 +1,23 @@
 //constantes
+
 const canvas = document.getElementById('juego');
 const context = canvas.getContext('2d');
 const nave = new Image();
 nave.src = './images/imagen1.png';
 const espacio = new Image();
 espacio.src = './images/imagen4.jpg';
-const disparo = new Image();
-disparo.src = "./images/disparo_bueno.png"
 
+//Variables
+
+var teclaIzquierda = 37;
+var teclaDerecha = 39;
+var teclaEspacio = 32;
+var teclaPulsada = null;
+var tecla = [];
+var x = 520;
+var y = 100;
+var tiempoBala = true;
+var balas_array = new Array();
 
 
 
@@ -28,85 +38,113 @@ const Bg = {
     }
 }
 
-//Shot
-
-
-
-//funciones
 
 
 //Jugador
-const player = {
-    height: 100,
-    width: 100,
-    x: 550,
-    y: 440,
-    dx: 0,
-    dy: 0,
-    speed: 20,
+class player {
 
-
-
-    imagenes() {
-        context.drawImage(nave, this.x, this.y, this.width, this.height);
-    },
-    newPos() {
-        this.x += this.dx;
-
-    },
-    detectWalls() {
-        if (player.x < 0) {
-            player.x = 0;
-        }
-
-        if ((player.x + player.width) > canvas.width) {
-            player.x = (canvas.width - player.width);
-
-        }
-
-    },
-
-
-}
-
-const shoot = {
-    height: 15,
-    width: 15,
-    x: 550,
-    y: 390,
-
-    imagen() {
-
-        context.drawImage(disparo, 585, 390, 15, 15);
+    constructor(x) {
+        this.x = x;
+        this.y = 440;
+        this.w = 100;
+        this.h = 100;
 
     }
 
+    dibuja(x) {
 
+        this.x = x;
+
+        context.drawImage(nave, this.x, this.y, this.w, this.h);
+    };
 }
 
-const moveRight = () => {
-
-    player.dx += player.speed;
+//Bala
+function Bala(x, y, w) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.dibuja = function() {
+        context.save();
+        context.fillStyle = "blue";
+        context.fillRect(this.x, this.y, this.w, this.w);
+        this.y = this.y - 6;
+        context.restore();
+    };
+    this.dispara = function() {
+        context.save();
+        context.fillStyle = "red";
+        context.fillRect(this.x, this.y, this.w, this.w);
+        this.y = this.y + 4;
+        context.restore();
+    };
 }
-const moveLeft = () => {
 
-    player.dx -= player.speed;
+/*************
+LISTENER
+**************/
+window.requestAnimationFrame = (function() {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function(callback) { window.setTimeout(callback, 17); }
+})();
+document.addEventListener("keydown", function(e) {
+    teclaPulsada = e.keyCode;;
+    tecla[e.keyCode] = true;
+});
+document.addEventListener("keyup", function(e) {
+    tecla[e.keyCode] = false;
+});
+
+
+function verifica(boton = false, codigo = 0) {
+    if (boton) {
+        teclaPulsada = codigo;
+        tecla[codigo] = true;
+    }
+    if (tecla[teclaDerecha]) x += 10;
+    if (tecla[teclaIzquierda]) x -= 10;
+    //Verifica cañon
+    console.log(x);
+    if (x > 1100) {
+
+        x = 1100;
+        console.log("entro");
+    }
+
+    if (x < 0) {
+        x = 0;
+    }
+
+
+    //Disparo
+    if (tecla[teclaEspacio]) {
+        if (tiempoBala == true) {
+            tiempoBala = false;
+            balas_array.push(new Bala(prueba.x + 48, prueba.y - 3, 5));
+            tecla[teclaEspacio] = false;
+            setTimeout(function() { tiempoBala = true; }, 300);
+        }
+    }
+    if (boton) {
+
+        tecla[codigo] = false;
+
+    }
 }
-const keyUp = () => {
-    player.dx = 0;
-}
 
-const keyDown = (event) => {
 
-    switch (event.key) {
-        case "ArrowRight":
-            moveRight();
-            break;
-        case "ArrowLeft":
-            moveLeft();
-            break;
-        default:
-            return;
+
+function pinta() {
+
+    //Balas
+    for (var i = 0; i < balas_array.length; i++) {
+        if (balas_array[i] != null) {
+            balas_array[i].dibuja();
+
+
+        }
     }
 
 }
@@ -116,15 +154,13 @@ const keyDown = (event) => {
 const startGame = () => {
     Bg.move();
     Bg.draw();
-
-    shoot.imagen();
-    player.imagenes();
-    player.newPos();
-    player.detectWalls();
+    prueba = new player(0);
+    prueba.dibuja(x);
+    verifica();
+    pinta();
     requestAnimationFrame(startGame);
 
 }
-startGame();
 
-document.addEventListener('keydown', keyDown);
-document.addEventListener('keyup', keyUp);
+
+startGame();
